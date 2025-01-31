ef9b1ba3e74c2ed9395f6c4c97f46aa91e19a35b
SrTorrda <41927844+SrTorrda@users.noreply.github.com>
2025-01-31 19:17:28 -0400

    Update finish-matches.js

--- scripts/finish-matches.js
+++ scripts/finish-matches.js
@@ -13,11 +13,11 @@ const {
 } = require('firebase/firestore');
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJh45lbjhCek0BP30K7YvSK-hG-8SeWa0',
-  authDomain: 'cards-against-humanity-16605.firebaseapp.com',
-  projectId: 'cards-against-humanity-16605',
-  storageBucket: 'cards-against-humanity-16605.appspot.com',
-  messagingSenderId: '1094359847041',
+  apiKey: 'AIzaSyASGjUugvR_zgp8xH-x8yA3FcM-FCcXR_I',
+  authDomain: 'ccch-26837.firebaseapp.com',
+  projectId: 'cch-26837',
+  storageBucket: 'cch-26837.appspot.com',
+  messagingSenderId: '16189043560',
   appId: '1:1094359847041:web:a8ecc0bd327d85ee5f3d80',
   measurementId: 'G-2GNVB3MBHM',
 };
